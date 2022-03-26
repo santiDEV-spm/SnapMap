d58159b185b2240d802d5b8da839e4beb0bc32c5
santiDev <androidsan94@gmail.com>
2022-03-25 19:33:38 -0600

    Capture layout

--- app/src/main/java/com/santidev/snapmap/Fragments/TitlesFragment.java
+++ app/src/main/java/com/santidev/snapmap/Fragments/TitlesFragment.java
@@ -2,9 +2,7 @@ package com.santidev.snapmap.Fragments;
 
 import android.app.ListFragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+
 
 import androidx.annotation.Nullable;
 
